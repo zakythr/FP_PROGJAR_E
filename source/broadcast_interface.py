a255cddb95ab9125619f4f9f64002153ae4700b7
yogamahottama <yoga.mahottama@gmail.com>
2020-05-15 16:38:34 +0800

    update broadcast interface
    nambah tombol cancel

--- source/broadcast_interface.py
+++ source/broadcast_interface.py
@@ -2,10 +2,13 @@ from tkinter import *
 from tkinter import ttk
 
 master = Tk()
+master.title("What's on your mind?")
 status = Label(master, text="Status: ", width=6)
 status.pack(side=LEFT, anchor=N, padx=5, pady=5)
 insertStatus = Text(master)
 insertStatus.pack(fill=BOTH, pady=5, padx=5, expand=True)
 tombolKirim = Button(master, text="Kirim", command=master.quit)
 tombolKirim.pack(side=RIGHT, padx=5, pady=5)
+tombolCancel = Button(master, text="Cancel", command=master.quit)
+tombolCancel.pack(side=RIGHT)
 master.mainloop()
