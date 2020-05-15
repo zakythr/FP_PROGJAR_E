bb15ceac6aca61e6d2466c8a188dea9efdcc32b5
yogamahottama <yoga.mahottama@gmail.com>
2020-05-15 10:55:32 +0800

    Create broadcast interface
    bisa nampilin box buat ngetik status

---
+++ source/broadcast_interface.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import tkinter as tk
+
+master = tk.Tk()
+tk.Label(master, text="Kirim Status").grid(row=0)
+
+e1 = tk.Entry(master)
+e1.grid(row=0, column=1)
+master.mainloop()
+
