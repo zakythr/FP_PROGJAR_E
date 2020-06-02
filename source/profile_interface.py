12494940ef32f0c223230556bb0c31e5cc448c57
Zaky Thariq H <zakythariq@gmail.com>
2020-06-02 11:19:17 +0700

    delete_onclosing

--- source/profile_interface.py
+++ source/profile_interface.py
@@ -6,10 +6,6 @@ import login_interface as login
 def runprofile(nama_akun):
     print(nama_akun)
     
-    def on_closing():
-        if messagebox.askokcancel("Keluar", "Apakah anda ingin keluar?"):
-            root.destroy()
-    
     root = Toplevel()
     root.title("Profil")
     root.geometry("350x350")
@@ -29,6 +25,5 @@ def runprofile(nama_akun):
     frame1.pack()
     frame2.pack()
     
-    root.protocol("WM_DELETE_WINDOW", on_closing)
     root.mainloop()
     
